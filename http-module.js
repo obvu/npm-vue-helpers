9dfbc50726451bf63a2c2e83f3f2ec57dc3f560a
Anton Morev <shikto@mail.ru>
2019-04-23 22:33:32 +0300

    auto select

--- http-module.js
+++ http-module.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const proxy = require('http-proxy-middleware')
 const app = express()
+const makeCache = require('nuxt-ssr-cache/lib/cache-builders')
 
 export default function SimpleModule (moduleOptions) {
   var defaultModuleOptions = {
@@ -12,6 +13,8 @@ export default function SimpleModule (moduleOptions) {
   // API PROXY HANDLER START
   const options = Object.assign({}, defaultModuleOptions, moduleOptions)
   if (options.apiProxy && this.options.env.siteApiHost) {
+    const cache = makeCache(this.options.cache.store)
+    console.log(cache)
     // app.use(express.json())
     app.use(bodyParser.urlencoded({ extended: false }))
     let baseProxyOptions = {
@@ -21,6 +24,14 @@ export default function SimpleModule (moduleOptions) {
     }
     let proxyOptions = Object.assign({}, baseProxyOptions, moduleOptions.proxyOptions)
     app.use('/prx/*', proxy(proxyOptions))
+    app.get('/core/purgeCache', (req, res) => {
+      cache.get('apiVersion').then(e => {
+        console.log(e)
+      })
+      // cache.reset()
+      console.log('cacheResetted')
+      res.send('cacheResetted')
+    })
 
     this.addServerMiddleware({ path: '/api', handler: app })
   }
